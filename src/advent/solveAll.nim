111aba818445f7f347b104f6b5bd2cdaa6402574
Dylan Vann <dylan@dylanvann.com>
2020-12-04 23:16:37 -0500

    Add solveAll

---
+++ src/advent/solveAll.nim
@@ -0,0 +1,15 @@
+import day01
+discard day01.solveA(readFile("tests/tday01.txt"))
+discard day01.solveB(readFile("tests/tday01.txt"))
+
+import day02
+discard day02.solveA(readFile("tests/tday02.txt"))
+discard day02.solveB(readFile("tests/tday02.txt"))
+
+import day03
+discard day03.solveA(readFile("tests/tday03.txt"))
+discard day03.solveB(readFile("tests/tday03.txt"))
+
+import day04
+discard day04.solveA(readFile("tests/tday04.txt"))
+discard day04.solveB(readFile("tests/tday04.txt"))
