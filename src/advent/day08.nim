1e29acdebdb5f3d33eb4bbcd3c9d072947078026
Dylan Vann <dylan@dylanvann.com>
2020-12-08 23:56:31 -0500

    Cleanup

--- src/advent/day08.nim
+++ src/advent/day08.nim
@@ -3,80 +3,44 @@ import sets
 import strscans
 import options
 
-proc getLastAccBeforeLoop(input: string): int =
-    let lines = input.splitLines
-    var foundLines = initHashSet[int]()
-    var acc = 0
-    var pos = 0
-    while true:
-        if foundLines.contains(pos):
-            return acc
-        else:
-            foundLines.incl(pos)
-        let line = lines[pos]
-        var instruction: string
-        var value: int
-        if line.scanf("$+ $i", instruction, value):
-            if instruction == "nop":
-                pos += 1
-                continue
-            if instruction == "acc":
-                acc += value
-                pos += 1
-                continue
-            if instruction == "jmp":
-                pos += value
-                continue
+proc parseLine(line: string): (string, int) =
+    var instruction: string
+    var value: int
+    if line.scanf("$+ $i", instruction, value): return (instruction, value)
 
-proc getAccAtEnd(lines: seq[string]): Option[int] =
+proc getAccAtEnd(lines: seq[string], allowCycle: bool): Option[int] =
     var foundLines = initHashSet[int]()
     var acc = 0
     var pos = 0
     while true:
-        if foundLines.contains(pos):
+        if pos == lines.len:
+            return some(acc)
+        elif foundLines.contains(pos):
+            if allowCycle: return some(acc)
             return none(int)
         else:
             foundLines.incl(pos)
-        if pos == lines.len:
-            return some(acc)
         let line = lines[pos]
-        var instruction: string
-        var value: int
-        if line.scanf("$+ $i", instruction, value):
-            if instruction == "nop":
-                pos += 1
-                continue
-            if instruction == "acc":
-                acc += value
-                pos += 1
-                continue
-            if instruction == "jmp":
-                pos += value
-                continue
+        let (instruction, value) = line.parseLine
+        case instruction:
+        of "nop": pos += 1
+        of "jmp": pos += value
+        of "acc": 
+            acc += value
+            pos += 1
 
 proc solveA*(input: string): int = 
-    return getLastAccBeforeLoop(input)
+    return getAccAtEnd(input.splitLines, true).get
 
 proc solveB*(input: string): int =
     let lines = input.splitLines
     for i in 0..(lines.len - 1):
         let line = lines[i]
-
-        # Parse
-        var instruction: string
-        var value: int
-        discard line.scanf("$+ $i", instruction, value)
-
-        # Modified
-        var newLines = lines
-        if instruction == "jmp":
-            newLines[i] = line.replace("jmp", "nop")
-        elif instruction == "nop":
-            newLines[i] = line.replace("nop", "jmp")
-        else:
-            continue
-
-        # Result
-        let accAtEnd = getAccAtEnd(newLines)
+        let (instruction, value) = line.parseLine
+        if not ["jmp", "nop"].contains(instruction): continue
+        var modifiedLines = lines
+        if instruction == "jmp": modifiedLines[i] = line.replace("jmp", "nop")
+        if instruction == "nop": modifiedLines[i] = line.replace("nop", "jmp")
+        let accAtEnd = getAccAtEnd(modifiedLines, false)
         if accAtEnd.isSome:
             return accAtEnd.get
